e6b4c4d33b79cb1cfcd127ef2622bf9828da7f19
beedsneeds <bharadhwajkram@gmail.com>
2025-11-20 01:36:02 -0500

    Forgive the spelling of compiled

--- Teeth.java
+++ Teeth.java
@@ -2,19 +2,18 @@
 
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
 public class Teeth {
+    static List<Integer> top;
+    static List<Integer> bottom;
 
-    public static int recursive(List<Integer> a, List<Integer> b) {
+
+    public static int recursiveTopDown(List<Integer> a, List<Integer> b) {
+        // n - i - a
         int n = a.size();
         int m = b.size();
 
@@ -27,11 +26,8 @@ public class Teeth {
     }
     
 
-    private static int minMaxHeight(int i, int j,
-                                     List<Integer> a,
-                                     List<Integer> b,
-                                     int[][] memo) {
-        if (memo[i][j] != -1L) {
+    private static int minMaxHeight(int i, int j, List<Integer> a, List<Integer> b, int[][] memo) {
+        if (memo[i][j] != -1) {
             return memo[i][j];
         }
 
@@ -44,13 +40,13 @@ public class Teeth {
 
         int bestPrev = Integer.MAX_VALUE;
 
-        if (i > 0) { // repeat b[j]
+        if (i > 0) {  // repeat b[j]
             bestPrev = Math.min(bestPrev, minMaxHeight(i - 1, j, a, b, memo));
         }
-        if (j > 0) { // repeat a[i]
+        if (j > 0) {   // repeat a[i]
             bestPrev = Math.min(bestPrev, minMaxHeight(i, j - 1, a, b, memo));
         }
-        if (i > 0 && j > 0) { // advance both
+        if (i > 0 && j > 0) {  // advance both
             bestPrev = Math.min(bestPrev, minMaxHeight(i - 1, j - 1, a, b, memo));
         }
 
@@ -58,17 +54,8 @@ public class Teeth {
         return memo[i][j];
     }
 
-    // Optional: convenience overload for arrays
-//    public static int recursive(int[] aArr, int[] bArr) {
-//        List<Integer> a = new ArrayList<>(aArr.length);
-//        List<Integer> b = new ArrayList<>(bArr.length);
-//        for (int x : aArr) a.add(x);
-//        for (int x : bArr) b.add(x);
-//        return recursive(a, b);
-//    }
 
-
-    static TeethInstance readTeethInput(String path) throws IOException {
+    static void parseInput(String path) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(path));
 
         String line = br.readLine();
@@ -81,27 +68,28 @@ public class Teeth {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        List<Integer> top = new ArrayList<>(n);
-        List<Integer> bottom = new ArrayList<>(m);
+        top = new ArrayList<>(n);
+        bottom = new ArrayList<>(m);
 
         line = br.readLine();
         st = new StringTokenizer(line);
         for (int i = 0; i < n; i++) {
             top.add(Integer.parseInt(st.nextToken()));
         }
+        System.out.println(top);
 
         line = br.readLine();
         st = new StringTokenizer(line);
         for (int i = 0; i < m; i++) {
             bottom.add(Integer.parseInt(st.nextToken()));
         }
-
+        System.out.println(bottom);
+        
         br.close();
-        return new TeethInstance(top, bottom);
     }
 
 
-    public void writeAlignmentToFile(String path, int height, int[] top, int[] bottom) throws IOException {
+    static void writeOutput(String path, int height, int[] top, int[] bottom) throws IOException {
         if (top.length != bottom.length) {
             throw new IllegalArgumentException("We messed up");
         }
@@ -127,44 +115,32 @@ public class Teeth {
         List<Integer> e = new ArrayList<>(Arrays.asList(1, 3, 2));
         List<Integer> f = new ArrayList<>(Arrays.asList(4, 5, 2));
 
+        // TODO change this before JAR
+        String cwd = System.getProperty("user.dir");
+        String inputPath  = String.valueOf(Path.of(cwd, "Java/src/", "input.txt"));
+        String outputPath = String.valueOf(Path.of(cwd, "Java/src/", "output.txt"));
+        System.out.println(inputPath); // TODO remove
 
-        if (args.length < 1) {
-            System.out.println("Usage: java ReadInput <input.txt>");
-            return;
-        }
-        String inputFile = args[0];
-        System.out.println(inputFile);
-//        TeethInstance readTeethInput(String path) throws IOException {
-        
-        TeethInstance teeth = readTeethInput(inputFile);
-        System.out.println(recursive(teeth.top, teeth.bottom));
-//
+        parseInput(inputPath);
+        int height = recursiveTopDown(top, bottom);
+        System.out.println(height);
+        // TODO remove vomit
+        int[] topArr = top.stream().mapToInt(i->i).toArray();
+        int[] bottomArr = bottom.stream().mapToInt(i->i).toArray();
 
-//        String contents = new String(Files.readAllBytes(Paths.get(inputFile)));
-//        System.out.println(contents);
-
-
-//        TeethInstance inst = readTeethInput(inputFile);
+        writeOutput(outputPath, height, topArr, bottomArr);
 
 
-        System.out.println(recursive(a, b));
-        System.out.println(recursive(c, d));
-        System.out.println(recursive(e, f));
+        System.out.println(recursiveTopDown(a, b));
+        System.out.println(recursiveTopDown(c, d));
+        System.out.println(recursiveTopDown(e, f));
 
     }
 }
 
 
 
-final class TeethInstance {
-    public final List<Integer> top;
-    public final List<Integer> bottom;
 
-    public TeethInstance(List<Integer> top, List<Integer> bottom) {
-        this.top = top;
-        this.bottom = bottom;
-    }
-}
 
 
 
